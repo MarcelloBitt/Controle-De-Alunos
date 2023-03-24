fd55d5f9f314904de0d21cf9614e2e51522af6bf
Marcello Bittencourt <108131718+MarcelloBitt@users.noreply.github.com>
2023-03-24 17:33:05 -0300

    added Redux feat

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaHome, FaSignInAlt, FaUserAlt } from "react-icons/fa";
+import { useSelector } from "react-redux";
+
 import { Nav } from "./styled";
 
 export default function Header() {
+  const botaoClicado = useSelector((state) => state.example.botaoClicado);
   return (
     <Nav>
       <Link to="/">
@@ -16,6 +19,7 @@ export default function Header() {
       <Link to="logout">
         <FaSignInAlt size={24} />
       </Link>
+      {botaoClicado ? "Clicado" : "Não clicado"}
     </Nav>
   );
 }
